db578271a68d67e4880a163a9382e74a2235d928
Davin Chang <davinchang12@gmail.com>
2022-03-19 15:16:57 +0700

    Add login controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [LoginController::class, 'index']);
+Route::post('/', [LoginController::class, 'authenticate']);
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
 });
