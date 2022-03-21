ac298cd6e1fdb863cd9b54545b582e8ce013badb
jevoncarlalim <jevoncarlalim@gmail.com>
2022-03-21 21:33:06 +0700

    curd_matkul_jadwal_kritiksaran

---
+++ app/Http/Controllers/KritikSaranController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KritikSaran;
+use Illuminate\Http\Request;
+
+class KritikSaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\KritikSaran $kritiksaran
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KritikSaran $kritiksaran)
+    {
+        //
+    }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\Models\KritikSaran $kritiksaran
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(KritikSaran $kritiksaran)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \App\Models\KritikSaran $kritiksaran
+    //  * @return \Illuminate\Http\Response
+    //  */
+    public function update(Request $request, KritikSaran $kritiksaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\KritikSaran $kritiksaran
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KritikSaran $kritiksaran)
+    {
+        //
+    }
+}
