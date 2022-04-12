aace234dbc3cf59748473079a1b48bfd12f95347
Davin Chang <davinchang12@gmail.com>
2022-04-12 19:59:43 +0700

    Set socas data

--- app/Http/Controllers/MatkulController.php
+++ app/Http/Controllers/MatkulController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Nilai;
 use App\Models\Jadwal;
+use App\Models\JenisSOCA;
 use App\Models\Matkul;
 use App\Models\Kelompok;
 use App\Models\NilaiTugas;
 use Illuminate\Http\Request;
+use App\Models\NilaiJenisSOCA;
 use Illuminate\Support\Facades\DB;
 
 class MatkulController extends Controller
@@ -58,6 +60,7 @@ class MatkulController extends Controller
             'user_id' => auth()->user()->id,
             'matkul_id' => $matkul->id 
         ];
+        
         $nilaitugas=  DB::table('nilai_tugas')
         ->join('nilais', 'nilais.id', '=', 'nilai_tugas.nilai_id')
         ->join('users', 'users.id', '=', 'nilais.user_id')
@@ -65,12 +68,23 @@ class MatkulController extends Controller
         ->where('matkul_id', '=', $matkul->id )
         ->get();
         $nilai = Nilai::where('matkul_id', $matkul->id)->pluck('id');
+
+        $socas = DB::table('jenis_s_o_c_a_s')
+            ->join('nilai_jenis_s_o_c_a_s', 'jenis_s_o_c_a_s.nilaijenissoca_id', '=', 'nilai_jenis_s_o_c_a_s.id')
+            ->join('nilai_s_o_c_a_s', 'nilai_jenis_s_o_c_a_s.nilaisoca_id', '=', 'nilai_s_o_c_a_s.id')
+            ->join('nilais', 'nilai_s_o_c_a_s.nilai_id', '=', 'nilais.id')
+            ->join('matkuls', 'nilais.matkul_id', '=', 'matkuls.id')
+            ->where('matkuls.id', $matkul->id)
+            ->where('nilai_jenis_s_o_c_a_s.namaanalisis', 'Kemampuan mengaplikasikan pengetahuan ilmu dasar untuk menjelaskan terjadinya penyakit  sesuai dengan skenario)')
+            ->get();
+
         return view('dashboard.nilai.dosen.index', [
             'kelompoks' => Kelompok::where($checkUserAndMatkul)->get(),
             'matkul_id' => $matkul->id,
             'siswas' => Kelompok::where('matkul_id', $matkul->id)-> get(),
             'nilaitugas' => $nilaitugas,
-            'namamatkul' => Matkul::where('id', $matkul->id)->pluck('namamatkul')
+            'namamatkul' => Matkul::where('id', $matkul->id)->pluck('namamatkul'),
+            'socas' => $socas
         ]);
     }
 
