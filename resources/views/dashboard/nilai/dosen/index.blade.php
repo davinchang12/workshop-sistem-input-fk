90349f7572ec21b8e357e38c98c88dd9832d999e
Davin Chang <davinchang12@gmail.com>
2022-04-15 11:55:36 +0700

    Add field lab nilai view

--- resources/views/dashboard/nilai/dosen/index.blade.php
+++ resources/views/dashboard/nilai/dosen/index.blade.php
@@ -385,6 +385,42 @@
                     </div>
                 </div>
             </form>
+            @if (count($fieldlabs) > 0)
+                <div class="mt-3 mb-3">
+                    <table class="table-bordered">
+                        <colgroup width="50"></colgroup>
+                        <colgroup width="200"></colgroup>
+                        <colgroup width="200"></colgroup>
+                        <colgroup width="200"></colgroup>
+                        <colgroup width="200"></colgroup>
+                        <colgroup width="200"></colgroup>
+                        <colgroup width="200"></colgroup>
+                        <colgroup width="200"></colgroup>
+                        <tr>
+                            <td height="40" align="center" valign=middle bgcolor="#FFE48F"><font color="#000000">No</font></td>
+                            <td align="center" valign=middle bgcolor="#FFE48F"><font color="#000000">Nama</font></td>
+                            <td align="center" valign=middle bgcolor="#FFE48F"><font color="#000000">Nim</font></td>
+                            <td align="center" valign=middle bgcolor="#FFE48F"><font color="#000000">Total Nilai Dosbing</font></td>
+                            <td align="center" valign=middle bgcolor="#FFE48F"><font color="#000000">Total Nilai Penguji</font></td>
+                            <td align="center" valign=middle bgcolor="#FFE48F"><font color="#000000">Total Nilai Dosen Luar</font></td>
+                            <td align="center" valign=middle bgcolor="#FFE48F"><font color="#000000">Nilai Akhir</font></td>
+                            <td align="center" valign=middle bgcolor="#FFE48F" style="width: 20%"><font color="#000000">Keterangan</font></td>
+                        </tr>
+                        @foreach ($fieldlabs as $fieldlab)
+                            <tr>
+                                <td height="19" align="center" valign=middle>{{ $loop->iteration }}</td>
+                                <td align="center" valign=middle>{{ $fieldlab->name }}</td>
+                                <td align="center" valign=middle>{{ $fieldlab->nim }}</td>
+                                <td align="center" valign=middle>{{ $fieldlab->total_nilai_dosbing }}</td>
+                                <td align="center" valign=middle>{{ $fieldlab->total_nilai_penguji }}</td>
+                                <td align="center" valign=middle>{{ $fieldlab->total_nilai_penguji_2 }}</td>
+                                <td align="center" valign=middle>{{ $fieldlab->nilai_akhir }}</td>
+                                <td align="center" valign=middle>{{ $fieldlab->keterangan }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            @endif
         </div>
         <div class="tab-pane fade" id="Ujian">
             <p>Ujian tab content ...</p>
