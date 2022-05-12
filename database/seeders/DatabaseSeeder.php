6c498a33251f571826f0e3f9167abceaf8da93e0
Davin Chang <davinchang12@gmail.com>
2022-05-12 14:56:08 +0700

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -909,98 +909,98 @@ class DatabaseSeeder extends Seeder
             'namapraktikum' => 'Histiologi'
         ]);
 
-        NilaiSOCA::create([
-            'nilai_lain_id' => 1,
-            'namasoca' => "SKA",
-            'nama_penguji' => "dr. A"
-        ]);
+        // NilaiSOCA::create([
+        //     'nilai_lain_id' => 1,
+        //     'namasoca' => "SKA",
+        //     'nama_penguji' => "dr. A"
+        // ]);
 
-        NilaiSOCA::create([
-            'nilai_lain_id' => 3,
-            'namasoca' => "SKA",
-            'nama_penguji' => "dr. A"
-        ]);
+        // NilaiSOCA::create([
+        //     'nilai_lain_id' => 3,
+        //     'namasoca' => "SKA",
+        //     'nama_penguji' => "dr. A"
+        // ]);
 
-        NilaiJenisSOCA::create([
-            'nilaisoca_id' => 1,
-            'namaanalisis' => "Kemampuan analisa masalah"
-        ]);
-        NilaiJenisSOCA::create([
-            'nilaisoca_id' => 1,
-            'namaanalisis' => "Kemampuan mengaplikasikan pengetahuan ilmu dasar untuk menjelaskan terjadinya penyakit  sesuai dengan skenario)"
-        ]);
-        NilaiJenisSOCA::create([
-            'nilaisoca_id' => 1,
-            'namaanalisis' => "Keterampilan saat presentasi"
-        ]);
-        NilaiJenisSOCA::create([
-            'nilaisoca_id' => 1,
-            'namaanalisis' => "Hasil Penilaian Keterampilan presentasi & sikap"
-        ]);
+        // NilaiJenisSOCA::create([
+        //     'nilaisoca_id' => 1,
+        //     'namaanalisis' => "Kemampuan analisa masalah"
+        // ]);
+        // NilaiJenisSOCA::create([
+        //     'nilaisoca_id' => 1,
+        //     'namaanalisis' => "Kemampuan mengaplikasikan pengetahuan ilmu dasar untuk menjelaskan terjadinya penyakit  sesuai dengan skenario)"
+        // ]);
+        // NilaiJenisSOCA::create([
+        //     'nilaisoca_id' => 1,
+        //     'namaanalisis' => "Keterampilan saat presentasi"
+        // ]);
+        // NilaiJenisSOCA::create([
+        //     'nilaisoca_id' => 1,
+        //     'namaanalisis' => "Hasil Penilaian Keterampilan presentasi & sikap"
+        // ]);
 
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 1,
-            "keterangan_soca" => "Overview Masalah",
-            "bobot" => 2,
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 1,
+        //     "keterangan_soca" => "Overview Masalah",
+        //     "bobot" => 2,
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
 
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 1,
-            "keterangan_soca" => "Analisis Masalah",
-            "bobot" => 4,
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 1,
+        //     "keterangan_soca" => "Analisis Masalah",
+        //     "bobot" => 4,
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
 
 
 
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 2,
-            "keterangan_soca" => "Fisiologi",
-            "bobot" => 2,
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 2,
-            "keterangan_soca" => "Patofisiologi SKA",
-            "bobot" => 2,
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 2,
-            "keterangan_soca" => "Gejala klinis dan pemeriksaan fisik",
-            "bobot" => 2,
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 3,
-            "keterangan_soca" => "Sikap",
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 3,
-            "keterangan_soca" => "Kemampuan berkomunikasi",
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 3,
-            "keterangan_soca" => "Sistematika penyajian",
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
-        JenisSOCA::create([
-            'nilaijenissoca_id' => 4,
-            "keterangan_soca" => "Hasil Penilaian Keterampilan presentasi & sikap",
-            "skor_soca" => 0,
-            "kepuasan_presentasi" => ""
-        ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 2,
+        //     "keterangan_soca" => "Fisiologi",
+        //     "bobot" => 2,
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 2,
+        //     "keterangan_soca" => "Patofisiologi SKA",
+        //     "bobot" => 2,
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 2,
+        //     "keterangan_soca" => "Gejala klinis dan pemeriksaan fisik",
+        //     "bobot" => 2,
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 3,
+        //     "keterangan_soca" => "Sikap",
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 3,
+        //     "keterangan_soca" => "Kemampuan berkomunikasi",
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 3,
+        //     "keterangan_soca" => "Sistematika penyajian",
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
+        // JenisSOCA::create([
+        //     'nilaijenissoca_id' => 4,
+        //     "keterangan_soca" => "Hasil Penilaian Keterampilan presentasi & sikap",
+        //     "skor_soca" => 0,
+        //     "kepuasan_presentasi" => ""
+        // ]);
         NilaiOSCE::create([
             'nilai_lain_id' => 2,
             'namaosce' => "PEMERIKSAAN FISIK PADA PAYUDARA",
