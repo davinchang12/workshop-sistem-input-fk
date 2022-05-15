cc838c6e2d4ee22f3a1b23442f13c5e7a3ded9ef
Davin Chang <davinchang12@gmail.com>
2022-05-15 09:36:40 +0700

    Move soalosce to new folder and delete soca

---
+++ resources/views/dashboard/osce/admin/export/soalosce.blade.php
@@ -0,0 +1,15 @@
+
+<table cellspacing="0" border="0">
+	<colgroup width="193"></colgroup>
+	<colgroup width="94"></colgroup>
+	<tr>
+		<td style="border: 1px solid black" colspan=2 height="21" align="center" valign=middle>Soal Lembar Penilaian OSCE</td>
+		</tr>
+	
+	<tr>
+		<td style="border: 1px solid black" align="center" valign=bottom>Aspek yang Dinilai</td>
+		<td style="border: 1px solid black" align="center" valign=bottom>Bobot</td>
+		
+	</tr>
+</table>
+
