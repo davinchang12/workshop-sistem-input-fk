0321528e47f3c847bdc9c0f3ac9f02bf879c0fc9
Davin Chang <davinchang12@gmail.com>
2022-05-25 21:02:31 +0700

    Add table rincian nilai akhir

---
+++ app/Models/RincianNilaiAkhir.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class RincianNilaiAkhir extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [
+        'id'
+    ];
+    protected $with = [
+        'nilai',
+    ];
+
+    public function nilai() {
+        return $this->belongsTo(Nilai::class, 'nilai_id');
+    }
+}
