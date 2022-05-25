5b8a9031f594dd129202a75b801091639dfc83cf
Davin Chang <davinchang12@gmail.com>
2022-05-25 21:10:31 +0700

    Add ip semester view

--- resources/views/dashboard/matkul/index.blade.php
+++ resources/views/dashboard/matkul/index.blade.php
@@ -6,6 +6,10 @@
     </div>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h3 class="h5">Pilih Mata Kuliah</h3>
+
+        @can('mahasiswa')
+            <h3 class="h5">Perkiraan IP Semester : </h3>
+        @endcan
     </div>
 
     @if (session()->has('success'))
